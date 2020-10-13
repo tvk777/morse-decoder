95fed4c6cf4bae2f1e5b97dfee70e264902880de
tvk777 <klimenko.tv7@gmail.com>
2020-10-13 22:23:29 +0300

    feat: start to resolve the task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const decodeLetter = str => {
+    for(let i; i < str.length; i++){
+        if(str[i]==='0') {
+            str.splice(0, 1);
+        } else {
+            return;
+        }
+    }
+    return str;
+}
+
 function decode(expr) {
-    // write your solution here
+    let decoded = '';
+    let decodedArray = [];
+    const words = expr.split('**********');
+    console.log(words);
+    const wordsByLetters = words.map(word => word.match(/.{1,10}/g));
+    wordsByLetters.forEach(word => {
+        decodedArray.push(word.map(letter => decodeLetter(letter)))
+    });
+    console.log(decodedArray);
 }
 
 module.exports = {
